4c85670fbab2c51bef947b040fc070928aaa1288
PNKim <paljapon.nait@gmail.com>
2024-06-20 16:39:10 +0700

    feat: Change title name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import "./App.css";
 import AttractionList from "./components/AttractionList";
 
 function App() {
+  useEffect(() => {
+    document.title = "Tourlist";
+  }, []);
   return (
     <div className="App flex flex-col items-center">
       <header class="text-center text-4xl text-blue-400 font-bold p-10 w-full">
